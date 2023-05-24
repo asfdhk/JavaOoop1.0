e09e4ba739ea010a39244493d1c2992d163e7bcb
asfdhk <134499499+asfdhk@users.noreply.github.com>
2023-05-24 15:53:46 +0300

    dz_1
    dz_1

---
+++ DZ_1/java/Product.java
@@ -0,0 +1,45 @@
+public class Product {
+
+    private int price;
+    private int weight;
+    private String color;
+
+    public Product(int price,int weight,String color){
+        this.price = price;
+        this.weight = weight;
+        this.color = color;
+    }
+
+    @Override
+    public String toString() {
+        return "Product{" +
+                "price=" + price +
+                ", weight=" + weight +
+                ", color='" + color + '\'' +
+                '}';
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public int getWeight() {
+        return weight;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public void setWeight(int weight) {
+        this.weight = weight;
+    }
+}
