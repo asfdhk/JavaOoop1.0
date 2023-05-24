3f80251e489858ece9b5407f19ed32cfbd535f29
asfdhk <134499499+asfdhk@users.noreply.github.com>
2023-05-24 15:53:03 +0300

    dr
    df

--- DZ_1/java/Main.java
+++
@@ -1,6 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        Product apple = new Product(100,20,"read");
-        Product cucumber = new Product(90,30,"green");
-    }
-}
